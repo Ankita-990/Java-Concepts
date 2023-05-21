49798ad269b1d14ca026fda1d8a01954a4235e90
Ankita Giroti <82829863+Ankita-990@users.noreply.github.com>
2023-05-21 23:07:09 +0530

    Fibonacci Series Done

---
+++ Day01/Fibonacci.java
@@ -0,0 +1,46 @@
+public class Fibonacci
+{
+    public static void main(String args[])
+    {
+
+        int pos = 0;
+        int value = 0;
+
+        value = fib(7);
+
+        System.out.println(value);
+
+    }
+
+    public static int fib(int pos)
+    {
+
+        if(pos == 0)
+            return 0;
+
+        if(pos == 1)
+            return 1;
+
+        return fib(pos - 1) + fib(pos - 2);
+
+
+
+
+
+
+
+
+
+
+
+        // int a = 0;
+        // int b = 1;
+        // int c = 0;
+        // for(int i=2; i<=pos; i++) {
+        //     c = a + b;
+        //     a = b;
+        //     b = c;
+        // }
+        // return c;
+    }
+}
---
+++ Day01/Memoization.java
@@ -0,0 +1,30 @@
+import java.
+
+class Memoiztion 
+{
+    public static void main(String args[])
+    {
+        int pos = 0;
+        int value = 0;
+
+        static Map<Integer, Integer> cache = new Map<>;
+
+        value = fib(8);
+        System.out.println(value);
+
+    }
+
+    public static int fib(int pos)
+    {
+        if(pos == 0)
+            return 0;
+
+        if(pos == 1)
+            return 1;
+
+        int result = fib(pos - 1) + fib(pos - 2);
+        cache.put(pos, result);
+
+        return result;
+    }
+}
